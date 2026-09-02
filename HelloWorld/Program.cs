2b82102fec9256d3c41f899fff436703101e142e
Chaw Theingi <chawtheingi17@gmail.com>
2026-09-02 14:05:01 +1200

    add first C# program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, Nice to meet you!");
+            Console.Write("How are you today?");
+        }
+    }
+}
